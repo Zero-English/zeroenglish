a3b481a3752aa0d407596273a9f07ae45de4e1ab
Tahmid Hasan <tahmidhasanpro@gmail.com>
2026-05-24 22:42:42 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Header />
-        <MobileBottomNav />
         {children}
         <Footer />
       </body>
