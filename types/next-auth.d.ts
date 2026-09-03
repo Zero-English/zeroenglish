18c1d5af032d7791ff0be27386d1ed3aa8ee20d8
Tahmid Hasan <tahmidhasanpro@gmail.com>
2026-09-03 09:54:13 +0600

    admin route protector

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -8,10 +8,18 @@ declare module "next-auth" {
             name?: string | null;
             email?: string | null;
             image?: string | null;
+            role?: "user" | "admin";
         };
     }
 
     interface User {
         id: number;
+        role?: "user" | "admin";
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        role?: "user" | "admin";
     }
 }
