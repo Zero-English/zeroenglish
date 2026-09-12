21655416d3816743fd5d7c1bb31b245709effbf3
Tahmid Hasan <tahmidhasanpro@gmail.com>
2026-09-12 09:40:14 +0600

    error fixed

--- utils/imagekit.ts
+++ utils/imagekit.ts
@@ -2,14 +2,26 @@ import ImageKit from "@imagekit/nodejs";
 
 const globalForImageKit = globalThis as unknown as { imagekit?: ImageKit };
 
-const imagekit =
-  globalForImageKit.imagekit ??
-  new ImageKit({
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForImageKit.imagekit = imagekit;
+function getImageKit(): ImageKit {
+  if (!globalForImageKit.imagekit) {
+    if (!process.env.IMAGEKIT_PRIVATE_KEY) {
+      throw new Error(
+        "IMAGEKIT_PRIVATE_KEY is not set; provide it to use the media library.",
+      );
+    }
+    globalForImageKit.imagekit = new ImageKit({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    });
+  }
+  return globalForImageKit.imagekit;
 }
+
+// Lazily create the ImageKit client on first use so importing this module does
+// not throw when IMAGEKIT_PRIVATE_KEY is missing at build/import time.
+const imagekit = new Proxy({} as ImageKit, {
+  get(_target, prop, receiver) {
+    return Reflect.get(getImageKit(), prop, receiver);
+  },
+});
 
 export default imagekit;
